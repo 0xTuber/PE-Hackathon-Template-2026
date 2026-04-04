91beb480e2add120b2d33a2c4f9c7c2c24c1de68
Chtaki <tarik152007@live.fr>
2026-04-03 22:30:34 -0400

    fix: remove intentional failure test

--- tests/test_fail.py
+++
@@ -1,7 +0,0 @@
-def test_intentional_failure():
-    """
-    THE GATEKEEPER!
-    This is an intentionally failing test so you can screenshot a blocked deploy!
-    Delete this file once you take your screenshot to let Jenkins pass.
-    """
-    assert False, "This test is designed to fail to demonstrate The Gatekeeper."
